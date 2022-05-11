30ca4b3e924f3d7ee58176de5aae169ed42fff19
themarkr <markrodriguezaa@gmail.com>
2022-05-11 17:24:06 -0400

    create states table

---
+++ migrations/20220511175454_create_states_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('states', function(table) {
+        table.increments('state_id', { primaryKey: true });
+        table.string('state_abrv')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('states')
+};
