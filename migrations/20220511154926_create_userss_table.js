90917d6cc4a937b976093dafeb51b86853498290
themarkr <markrodriguezaa@gmail.com>
2022-05-15 02:44:20 -0400

    modified max password length to store hashed passwords

--- migrations/20220511154926_create_userss_table.js
+++ migrations/20220511154926_create_userss_table.js
@@ -5,10 +5,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
         table.increments('user_id', { primaryKey: true });
-        table.string('first_name', 20);
-        table.string('last_name', 20);
+        table.string('first_name', 20).notNullable();
+        table.string('last_name', 20).notNullable();
         table.string('email').unique().notNullable();
-        table.string('password', 32);
+        table.string('password').notNullable();
         table.string('profile_pic_link', 600);
         table.boolean('is_lawyer');
         table.string('firm').nullable();
